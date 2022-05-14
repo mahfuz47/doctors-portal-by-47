421dddd7e229ce48c26f98c0e5e88e5bb0a9587c
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-14 17:29:26 +0600

    first commit

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InfoCard from "./InfoCard";
+import clock from "../../assets/icons/clock.svg";
+
+const Info = () => {
+  return (
+    <div>
+      <InfoCard img={clock}></InfoCard>
+    </div>
+  );
+};
+
+export default Info;
